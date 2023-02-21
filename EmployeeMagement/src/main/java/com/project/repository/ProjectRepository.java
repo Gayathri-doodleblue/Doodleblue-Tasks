3f81709b45395481a8dba7c7df7a36225e496da6
Gayathri-doodleblue <123168770+Gayathri-doodleblue@users.noreply.github.com>
2023-02-21 17:49:48 +0530

    Delete EmployeeMagement directory

--- EmployeeMagement/src/main/java/com/project/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.model.Project_Info;
-
-public interface ProjectRepository extends JpaRepository<Project_Info, Integer> {
-
-}
